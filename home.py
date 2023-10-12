1eaab772e1f41f9cc6c82835d7e4de0583cea814
thoai.nguyen <thoaint.dev@gmail.com>
2023-10-12 09:30:43 +0700

    upgrade demo

--- home.py
+++ home.py
@@ -2,6 +2,7 @@ import streamlit as st
 from htmlTemplates import css, bot_template, user_template
 from fn_ask_sql_database import handle_userinput_sql_database, config_sql_database
 from fn_ask_upload_file import get_ask_document
+from fn_get_type_file import get_file_upload
 
 def get_data_source():
         rdo_datasource = st.radio(
@@ -11,16 +12,22 @@ def get_data_source():
             horizontal = True,)
         return rdo_datasource
 
-def ask_question_user(question_data_source, pdf_file):
+def get_type_file():
+        rdo_typefile = st.radio(
+            "Please choose type file upload!",
+            ["File PDF", "File Text", "File Word"],
+            captions = [".pdf", ".txt", ".docx"],
+            horizontal = True,)
+        return rdo_typefile
+
+def ask_question_user(question_data_source, file_upload, radio_type_file):
     user_question = st.text_input("Ask a question: " + question_data_source, placeholder="Input search")  
     if user_question:
         if question_data_source == "SQL database":
             ask_question_sqldatabase(user_question) 
             handle_userinput(user_question)
         elif question_data_source == "Upload file":
-            if (pdf_file != ""):
-                get_ask_document(pdf_file)
-                handle_userinput(user_question)
+            handle_userinput(user_question)
      
 def ask_question_sqldatabase(user_question):
     res = handle_userinput_sql_database(user_question)
@@ -30,9 +37,6 @@ def ask_question_sqldatabase(user_question):
         "{{MSG}}", config_sql_database.run(res)), unsafe_allow_html=True)
     return res
 
-def ask_question_file(files):
-    get_ask_document(files)
-            
 def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
@@ -44,6 +48,14 @@ def handle_userinput(user_question):
             st.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
 
+def get_radio_type_file(upload_type_file):
+    if upload_type_file == "File PDF":
+        type_upload = "pdf"
+    elif upload_type_file == "File Text":
+        type_upload = "txt"
+    elif upload_type_file == "File Word":
+        type_upload = "doc"
+    return type_upload
 def main():
 
     st.set_page_config(page_title="ChatBot",
@@ -54,14 +66,17 @@ def main():
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
+    radio_type_file = "pdf"
     with st.sidebar:
+        upload_type_file = get_type_file()
+        radio_type_file = get_radio_type_file(upload_type_file)
         st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        file_uploader = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True, type=radio_type_file)
         if st.button("Process"):
             with st.spinner("Processing"):
-                 st.write(pdf_docs)
-    rs_ask = ask_question_user(question_data_source, pdf_docs)
+                 get_ask_document(file_uploader, radio_type_file)
+    rs_ask = ask_question_user(question_data_source, file_uploader, radio_type_file)
 
 if __name__ == '__main__':
     main()
